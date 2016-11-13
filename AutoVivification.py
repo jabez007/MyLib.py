5bbf11e1656766735a3e8d8468bbaddb0ccd33a9
jabez007 <jabez.m.007@gmail.com>
2016-11-13 14:48:37 -0600

    adding the ability the save to and load from json files

--- AutoVivification.py
+++ AutoVivification.py
@@ -6,6 +6,12 @@ class AutoVivification(dict):
     Implementation of perl's autovivification feature, combined with a
     data structure similar to MUMPS/Cache arrays
     """
+    null = None  # float('nan')
+
+    def __init__(self, iterable=None):
+        if iterable:
+            # dict.__init__(self, iterable, **kwargs)  # can't find a way to run this without throwing error yet
+            AutoVivification._load_dict_(iterable, self)
     '''
     def __getitem__(self, key):
         try:
@@ -20,45 +26,71 @@ class AutoVivification(dict):
         return value
         
     def __setitem__(self, key, value):
-        if isinstance(value, type(self)):
+        if isinstance(value, type(self)) or key == AutoVivification.null:
             dict.__setitem__(self, key, value)
         else:
-            dict.__setitem__(self[key], None, value)
+            dict.__setitem__(self[key], AutoVivification.null, value)
             
     def get_item(self, *args):
         obj = self
         for arg in args:
             obj = obj[arg]
-        return obj[None]
+        return obj[AutoVivification.null]
         
     def set_item(self, keys, value):
         obj = self
         for key in keys[:-1]:
             obj = obj[key]
         obj[keys[-1]] = value
-        
+
+    def save(self, file_path):
+        dump_string = json.dumps(self,
+                                 sort_keys=True, indent=4)
+        with open(file_path, 'w') as output:
+            output.write(dump_string)
+
+    @staticmethod
+    def load(file_path):
+        loaded_viv = AutoVivification()
+
+        with open(file_path, 'r') as input_file:
+            loaded = json.load(input_file)
+
+        AutoVivification._load_dict_(loaded, loaded_viv)
+
+        return loaded_viv
+
+    @staticmethod
+    def _load_dict_(iterable, out_object):
+        for k, v, in iterable.iteritems():
+            if k == 'null':
+                k = None
+
+            if isinstance(v, dict):
+                AutoVivification._load_dict_(v, out_object[k])
+            else:
+                out_object[k] = v
+
 # # # #
 
 
 if __name__ == "__main__":
     from time import time
-    
-    test_viv = AutoVivification()
-    
     start = time()
-    
-    test_viv["Hello"] = "a"
-    # print test_viv
-    
-    test_viv.set_item(["Hello", "World"], "b")    
-    print test_viv.get_item("Hello", "World")
-    
-    test_viv["Hello"]["Good Bye"] = "c"
-    print test_viv
-    
-    n = 1000000
-    test_viv.set_item([i for i in range(n)], "long test")
-    print test_viv.get_item(*[i for i in range(n)])
+
+    iterate = {'a': 'Hello', 'b': 'World'}
+    test_init = AutoVivification(iterate)
+    print test_init
+
+    test_save = AutoVivification()
+    test_save['a'] = "hello"
+    test_save['b'] = "world"
+    test_save['a']['b'] = "hello world"
+    print test_save.get_item('a', 'b')
+    test_save.save("test.json")
+
+    test_load = AutoVivification.load('test.json')
+    print test_load.get_item('a', 'b')
 
     finished = time() - start
     print "finished in %f seconds" % finished
